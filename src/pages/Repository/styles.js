e6f3453eea77554f764242586f686c79293fd577
Evandro Custodio <evandrocustodio@gmail.com>
2019-09-15 00:56:26 -0300

    Projeto finalizado - Fase 3 - Primeiro projeto em ReactJs

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+export const IssuesList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+li {
+  display: flex;
+  padding: 15px 10px;
+  border: 1px solid #eee;
+  border-radiu s: 4px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  & + li {
+    margin-top: 10px;
+  }
+
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 10px;
+    p{
+      margin-top: 5px;
+      font-size: 12px;
+      color: #999;
+    }
+    strong {
+    font-size: 16px;
+
+      span {
+        font-size: 12px;
+        color: #7159;
+        border: 1px solid #ccc;
+        background: #ccc;
+        border-radius: 5px;
+        max-width: 90px;
+        margin-left:10px;
+        padding: 2px;
+      }
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #7159c1;
+        }
+      }
+
+    }
+  }
+`;
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+  p {
+    margin-top: 5px;
+    line-height: 1.4;
+    color: #666,
+    font-size: 14px;
+    text-align: center;
+    max-width: 400px;
+
+  }
+`;
